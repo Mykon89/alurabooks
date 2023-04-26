0638abfc414cfe385ab79a3faf62718a930fe0e3
Mykon <helvis89@gmail.com>
2023-04-26 17:56:16 -0300

    first commit

---
+++ src/componentes/IconesHeader/index.js
@@ -0,0 +1,17 @@
+import './estilo.css';
+import perfil from '../../imagens/perfil.svg';
+import sacola from '../../imagens/sacola.svg';
+
+const icones = [perfil, sacola];
+
+export default function IconesHeader() {
+  return (
+    <ul className="icones">
+      {icones.map(icone => (
+        <li className="icone">
+          <img src={icone} alt="alt" />
+        </li>
+      ))}
+    </ul>
+  );
+}
