5103fb9bd28dbda0e4155581df901a46bfe0e543
Mykon <helvis89@gmail.com>
2023-04-27 17:48:34 -0300

    conclusao da primeira parte do curso

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,13 +1,22 @@
+import styled from 'styled-components';
 import logo from '../../imagens/logo.svg';
-import './estilo.css';
+
+const LogoContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+`;
+
+const LogoImage = styled.img`
+  margin-right: 10px;
+`;
 
 export default function Logo() {
   return (
-    <div className="logo">
-      <img src={logo} alt="logo" className="logo-img" />
+    <LogoContainer>
+      <LogoImage src={logo} alt="logo" />
       <p>
         <strong>Alura</strong>Books
       </p>
-    </div>
+    </LogoContainer>
   );
 }
