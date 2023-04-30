d090e9afabbe9e40758f256385d0b78001e77137
Mykon89 <helvis89@gmail.com>
2023-04-30 19:09:31 -0300

    api favoritos

---
+++ src/rotas/Home.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import Pesquisa from '../componentes/Pesquisa';
+import UltimosLancamentos from '../componentes/UltimosLancamentos';
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+`;
+
+export default function Home() {
+  return (
+    <AppContainer>
+      <Pesquisa />
+      <UltimosLancamentos />
+    </AppContainer>
+  );
+}
