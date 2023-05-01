6960baf6b5837310b0fbd40b52caea766f7081d5
Mykon89 <helvis89@gmail.com>
2023-05-01 14:58:42 -0300

    componentes

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,23 @@
+import Logo from '../Logo';
+import OpcoesHeader from '../OpcoesHeader';
+import IconesHeader from '../IconesHeader';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const HeaderContainer = styled.header`
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+`;
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <OpcoesHeader />
+      <IconesHeader />
+    </HeaderContainer>
+  );
+}
